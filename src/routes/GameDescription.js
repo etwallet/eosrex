26a4d772b5c8ecab3695004399695a8f7812abbc
bille18 <flying_why17@outlook.com>
2019-04-24 09:55:35 +0800

    描述页面，完善

--- src/routes/GameDescription.js
+++ src/routes/GameDescription.js
@@ -27,8 +27,8 @@ class GameDescription extends React.Component {
       <p style={styles.explaintext}>1、在进行买卖REX之前，用户必须要对至少21个节点投过票或代理投票，并且买入4天后方可卖出，之后就不需要进行节点投票。</p>
       <p style={styles.explaintext}> 2、如果REX里面没有足够的EOS，则排入队列，等到EOS足够再卖出，并且卖出时才计算REX价格，用户是可以取消尚未成交的卖出订单。</p>
       {this.state.lease &&
-        <div style={styles.rootDiv}>
-        <p style={styles.description}>租赁介绍</p>
+      <div >
+        <p style={styles.descriptiontitle}>租赁介绍</p>
         <p style={styles.explaintext}>1、您可以根据自己的需要填写足量的CPU，NET,租用时长，默认接收账户是您当前使用的账户，可给当前账户租赁，还可以帮助朋友租赁，
              默认接收账户是您当前使用账户，您可以修改为朋友账户，填写完成后，在订单确认里您可以看到当前可以租用的EOS金额，确认无误后，点击一键租赁完成资源租赁。</p>
       </div>
@@ -59,5 +59,11 @@ const styles = {
     fontSize: Auto.WHT(28), 
     lineHeight: Auto.WHT(36), 
     paddingTop: Auto.WHT(30),
+  },
+  descriptiontitle: {
+    color: '#000000', 
+    fontSize: Auto.WHT(48), 
+    lineHeight: Auto.WHT(67), 
+    paddingTop: Auto.WHT(70),
   },
 }
